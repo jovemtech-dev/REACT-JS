03b04ccf3bb11965017f3f66f38b057461b623ac
jovemtech-dev <jovemtechgrupo@gmail.com>
2025-10-30 14:50:25 -0300

    atualização dos arquivos

--- Dev/codeconnect/src/App.jsx
+++ Dev/codeconnect/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
 import Sidebar from './components/Sidebar'
+import BarraDePesquisa from './components/BarraDePesquisa'
 
 function App() {
   return (
+    <div className='container'>
     <Sidebar />
+    <BarraDePesquisa />
+    </div>
   )
 }
 
