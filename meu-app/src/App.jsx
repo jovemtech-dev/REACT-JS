4dbe9395981088dc6e49e1b88cf7c16412476c53
jovemtech-dev <jovemtechgrupo@gmail.com>
2025-11-03 15:04:42 -0300

    novo projeto

---
+++ meu-app/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import './Componentes/Banner/Banner.jsx'
+
+function App() {
+  return (
+    
+  )
+}
+
+export default App
---
+++ meu-app/src/Componentes/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import bannerImage from './img/banner'
+
+function Banner() {
+    return(
+        <div>
+            <img src={bannerImage} alt="Banner principal da pagina" />
+        </div>
+    )
+}
+
+export default Banner
